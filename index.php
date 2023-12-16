093d579ace85d35283c13a38648ec6eb20fbca09
azaysyaza <153976504+azaysyaza@users.noreply.github.com>
2023-12-16 14:57:50 +0800

    index.php new upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+	  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+
+	echo "<h1>Alhamdulillah</h2>";
+	
+?>
+
+</body>
+</html>
